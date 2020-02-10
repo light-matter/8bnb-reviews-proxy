09b193e897c5db076ec5b41f4c98e4c5ddf7c311
almostgone47 <almostgone47@hotmail.com>
2020-02-09 18:18:43 -0800

    added routes to proxy server

--- 8bnb-reviews-proxy/server/index.js
+++ 8bnb-reviews-proxy/server/index.js
@@ -1,13 +1,94 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const axios = require('axios');
+const request = require('request');
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.use(express.static('./public'));
+// Middleware
+app.use(morgan('dev')); // for logging http requests to the terminal
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use('/', express.static(path.join(__dirname, '../public'))); // for serving static files
 
-// app.get('localhost:3001/reviews', (req, res) => {
 
-// })
+// Proxy requests to modules
+const reservationModuleUrl = 'http://ec2-18-221-158-53.us-east-2.compute.amazonaws.com';
+const reviewsModuleUrl = 'http://ec2-18-216-209-189.us-east-2.compute.amazonaws.com:3000'; // add routes
+const calendarModuleUrl = 'http://ec2-54-153-109-129.us-west-1.compute.amazonaws.com:3000'; // add routes
+const similarHomesModuleUrl = 'http://ec2-54-183-237-192.us-west-1.compute.amazonaws.com:3001'; // change and add routes
 
+// Forward requests for a space's info to reservations module
+app.get('/spaces', function (req, res) {
+  request(`${reservationModuleUrl}/spaces?id=${req.query.id}`, function (error, response, body) {
+    if (error) {
+      console.error('error:', error);
+    } else {
+      console.log('statusCode:', response && response.statusCode);
+      res.send(body);
+    }
+  });
+});
 
+// Forward requests for a list of reservations to reservations module
+app.get('/reservations', function (req, res) {
+  request(`${reservationModuleUrl}/reservations?spaceId=${req.query.spaceId}`, function (error, response, body) {
+    if (error) {
+      console.error('error:', error);
+    } else {
+      console.log('statusCode:', response && response.statusCode);
+      res.send(body);
+    }
+  });
+});
 
-app.listen(port, () => console.log('Server listening on port ' + port));
+// Forward requests to post a reservation to reservations module server (WIP)
+app.post('/reservations', function (req, res) {
+  request(`${reservationModuleUrl}/reservations`, function (error, response, body) {
+    if (error) {
+      console.error('error:', error);
+    } else {
+      console.log('statusCode:', response && response.statusCode);
+      res.send(body);
+    }
+  });
+});
+
+// Forward requests to get a list of homes to Will's similar homes module
+app.get('/homes', function (req, res) {
+  request(`${similarHomesModuleUrl}/homes`, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('statusCode:', response && response.statusCode);
+      res.send(body);
+    }
+  })
+});
+
+// Forward requests to get a list of reviews to Jeremiah's reviews module
+app.get('/reviews', function (req, res) {
+  request(`${reviewsModuleUrl}/reviews`, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('statusCode:', response && response.statusCode);
+      res.send(body);
+    }
+  })
+});
+
+// Forward requests to get a space's reservations to Vikas' calendar module
+app.get('/data/:id', function (req, res) {
+  request(`${reviewsModuleUrl}/data/${req.params.id}`, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('statusCode:', response && response.statusCode);
+      res.send(body);
+    }
+  })
+});
+
+app.listen(port, () => console.log(`Proxy server listening on port ${port}!`));
